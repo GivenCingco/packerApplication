314c143294373c0939ebe06df323fbbe00dd703e
GivenCingco <givencingco@gmail.com>
2023-05-29 13:59:45 +0200

    Should render title in a h2 tag test works

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,8 +1,13 @@
-import { TestBed } from '@angular/core/testing';
+import { TestBed, async } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
+import { DebugElement } from '@angular/core';
 
 describe('AppComponent', () => {
+  let el:DebugElement;
+
+
+//This our to be tested unit, we
   beforeEach(() => TestBed.configureTestingModule({
     imports: [RouterTestingModule],
     declarations: [AppComponent]
@@ -14,16 +19,14 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'packer'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('packer');
+
+  //Test the app if it render
+  it('should render title in a h2 tag', async() => {
+    let fixture = TestBed.createComponent(AppComponent);
+    fixture.detectChanges();
+    let compiled = fixture.debugElement.nativeElement;
+    expect(compiled.querySelector('h2').textContent).toContain('Welcome to packer application')
+    
   });
 
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('packer app is running!');
-  });
 });
--- src/app/packer/packer.component.spec.ts
+++ src/app/packer/packer.component.spec.ts
@@ -1,21 +1,55 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { PackerComponent } from './packer.component';
+import { DebugElement } from '@angular/core';
+import { By } from '@angular/platform-browser';
 
 describe('PackerComponent', () => {
   let component: PackerComponent;
   let fixture: ComponentFixture<PackerComponent>;
+  let el:DebugElement; 
 
-  beforeEach(() => {
-    TestBed.configureTestingModule({
-      declarations: [PackerComponent]
-    });
-    fixture = TestBed.createComponent(PackerComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+  // beforeEach(() => {
+  //   TestBed.configureTestingModule({
+  //     declarations: [PackerComponent]
+  //   });
+  //   fixture = TestBed.createComponent(PackerComponent);
+  //   component = fixture.componentInstance;
+  //   fixture.detectChanges();
+  // });
+
+  // beforeEach(() => {
+  //   TestBed.configureTestingModule({
+  //     declarations: [PackerComponent]
+  //   }).compileComponents().then(() => {
+  //    fixture = TestBed.createComponent(PackerComponent);
+  //    component = fixture.componentInstance;
+  //   })
+  // })
+
+  beforeEach(waitForAsync(
+    () => {
+      TestBed.configureTestingModule({
+        declarations: [PackerComponent]
+      }).compileComponents().then(() => {
+       fixture = TestBed.createComponent(PackerComponent);
+       component = fixture.componentInstance
+       el = fixture.debugElement;
+      })
+    }
+  ))
+
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+
+  // it('should have correct contents', () => {
+  //  let heroElements = el.queryAll(By.css('p'))
+  //   expect(heroElements[0].nativeElement.textContent).toBe('Home works!')
+  //   let buttonElements = el.queryAll(By.css('btn-get-started'));
+  //   expect(buttonElements[0].nativeElement.textContent).toBe('Welcome to')
+
+  // })
 });
